97ac11b8a16ffd73480cc52de8028db38bc71a26
Kerslager-J-51ca <Kerslager-J-51ca@a2-07-mti.skynet.tul.cz>
2024-12-12 09:29:30 +0100

    testing push

---
+++ src/kerslager/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kerslager;
+
+/**
+ *
+ * @author Kerslager-J-51ca
+ */
+public class main {
+    public static void main(String[] args) {
+        
+    }
+}
