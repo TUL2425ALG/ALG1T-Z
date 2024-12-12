559eb20d83bbdd0fad6af87dad9bd68071311e56
Chynchyk-A-734b <Chynchyk-A-734b@a2-19-mti.mshome.net>
2024-12-12 12:01:11 +0100

    odevzdane

---
+++ src/main/java/Chynchyk/Reputation.java
@@ -0,0 +1,14 @@
+package Chynchyk;
+
+import java.util.Scanner;
+
+public class Reputation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A[];
+        while (true){
+            A[] = sc.nextInt();
+            
+        }
+    }
+}
