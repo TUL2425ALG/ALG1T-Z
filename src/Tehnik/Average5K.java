2f61d74e701a11ef5a6bf21f39871028784aed62
Tehnik-M-1424 <Tehnik-M-1424@a2-15-mti.skynet.tul.cz>
2024-12-12 12:01:10 +0100

    odevzdane

---
+++ src/Tehnik/Average5K.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tehnik;
+
+/**
+ *
+ * @author Tehnik-M-1424
+ */
+import java.util.Scanner;
+public class Average5K {
+    // 22,6 21,8 20,03 24,5 21,8 28,4 22,5 23,9 22,9 21,5
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        while(true){
+            final int DISTANCE = 1;
+            System.out.println("Zadej pocet behu: ");
+            int count = sc.nextInt();
+
+            if(count <= 0){ 
+                break;
+            }
+            
+            double[] runs = new double[count];
+
+            runs = FillArr(runs);
+            System.out.format("Prumerne jste 1 km zabehl za %.2f", Avg(runs));
+            System.out.println("");
+        }
+    }
+    
+   
+    
+    public static double[] FillArr (double[] arr)
+    {
+        System.out.println("Zadej jednotlive casy behu: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextDouble();
+        }
+        return arr;
+    }
+    
+    public static void PrintArr (double[] arr)
+    {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    
+    public static double Max (double[] arr)
+    {
+        double max = arr[0];
+        double min_val = Double.MIN_VALUE; // INTEGER.MINVAL
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > min_val)
+            {   
+                min_val = arr[i];
+            }
+            
+        }
+        max = min_val;
+        return max;
+    }
+    
+    public static double Min (double[] arr)
+    {
+        double min = arr[0];
+        double max_val = Double.MAX_VALUE; //INTEGER.MAXVAL
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<max_val)
+            {   
+                max_val = arr[i];
+            }
+            
+        }
+        min = max_val;
+        return min;
+    }
+    
+    public static double Avg (double[] arr)
+    {
+        double max = Max(arr);
+        double min = Min(arr);
+        double max_km = max/5.0;
+        double min_km = max/5.0;
+        double sum = 0;
+        double avg = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
+        sum = sum - max_km - min_km; 
+        avg = sum/((double)arr.length-2);
+        return avg;
+
+    }
+}
---
+++ src/Tehnik/Reputation.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tehnik;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tehnik-M-1424
+ */
+public class Reputation {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        // 0 3 -5 2 0 4 -3 5 0 
+        
+        int[][] mat = Input();
+        PrintMatrix(mat);
+        UserAvgRep(mat);
+        int[] reputations = CountRep(mat);
+        ControverseUser(reputations);
+        //TODO Pratelske dvojice
+        System.out.println("Platforma "+ (IsPlatformPositive(mat) ? "je ":"neni ") + "pozitivni" );
+        
+    }
+    
+    public static int[][] Input()
+    {
+        System.out.println("Zadej pocet uzivatelu: ");
+        int n = sc.nextInt();
+        int[][] reviews = new int[n][n];
+        System.out.println("Zadej hodnotici matici: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                reviews[i][j] = sc.nextInt();
+            }
+        }
+        return reviews;
+    }
+    public static void PrintMatrix(int[][] arr)
+    {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                //System.out.print(arr[i][j]);   
+                if (arr[i][j] < 0)
+                {
+                   System.out.print(" "); 
+                }else{
+                    System.out.print("  ");
+                }
+                System.out.print(arr[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static void UserAvgRep(int[][] arr)
+    {
+        double avg = 0; 
+        
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j){
+                    sum += arr[i][j];
+                }
+            }
+            avg = (double)sum/((double)arr.length-1);
+            System.out.println("Avg uzivatele " + (i+1) + " je " +avg);
+            
+        }
+   
+    }
+    
+    public static void ControverseUser (int[] arr)
+    {
+        int max = arr[0];
+        int uzivatel = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (Math.abs(arr[i]) > max)
+            {
+                max = Math.abs(arr[i]);
+                uzivatel = i;
+            }
+        }
+        System.out.println("Nejvice kontroverzni uzivatel je " + uzivatel + " Ma rozdil " + max);
+    }
+    
+    public static int[] CountRep(int[][] arr)
+    {
+        int reputation = 0;
+        
+        int[] reputations = new int[arr.length+1];
+        
+        for (int i = 0; i < arr.length; i++) {
+            //int reputation = 0;
+            int min = 0;
+            int max = 1000;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i][j] > min)
+                {
+                    min = arr[i][j];
+                }
+                if (arr[i][j] < max)
+                {
+                    max = arr[i][j];
+                }
+            }
+            reputation = max-min;
+            //System.out.println("Reputation " + (i+1) + reputation);
+            reputations[i+1] = reputation;
+        }
+        return reputations;
+    }
+    
+    public static void PrintArr (int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    
+    public static boolean IsPlatformPositive(int[][] arr)
+    {
+        boolean status = false;
+        for (int i = 0; i < arr.length; i++) {
+            status = false;
+            for (int j = 0; j < arr.length; j++) {
+                if(arr[i][j] > 0)
+                {
+                    status = true;
+                }
+                
+            }
+            
+        }
+        
+        return status;
+    }
+}
