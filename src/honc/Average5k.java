c01fe1e850e8561ade962976010631a9130379bd
Honc-F-e282 <Honc-F-e282@a2-06-mti.mshome.net>
2024-12-12 12:01:08 +0100

    Odevzdane

---
+++ src/honc/Average5k.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package honc;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Honc-F-e282
+ */
+public class Average5k {
+    public static void main(String[] args) {
+        while(true){
+        Scanner sc = new Scanner(System.in);
+        double suma = 0;
+        int length = 1;
+        System.out.println("Zadej pocet behu:");
+        int runs = sc.nextInt();
+        if(runs<=0)break;
+        System.out.println("Zadej casy behu:");
+        double[] a = new double[runs];
+        for (int i = 0; i < runs; i++) {
+            a[i] = sc.nextDouble();
+            suma = suma + a[i];
+        }
+        
+        System.out.println("Pouzite hodnoty");
+        System.out.print("{");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + (i == a.length-1?"":","));
+        }
+        System.out.print("}");
+        System.out.println("");
+        
+        double min = Double.MAX_VALUE;
+        double max = Double.MAX_VALUE*-1; //zatim
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+            if (a[i] > max) {
+                max = a[i];
+            } 
+        }
+        double mean = (suma-min-max)/(runs-2);
+        mean = mean*((double)length/5);
+        //System.out.println("Prumerne jste "+length+" zabehl za "+ mean + "min"); //format
+        System.out.format("Prumerne jste  %d kilometr zabehl za %.2f min",length,mean);
+        System.out.println();
+    }}
+}
---
+++ src/honc/Reputation.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package honc;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Honc-F-e282
+ */
+public class Reputation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadejte pocet uzivatelu:");
+        int count = sc.nextInt();
+        int[][] A = new int[count][count];
+        
+        System.out.println("Zapis hodnotici matici:");
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                A[i][j] = sc.nextInt();
+            }         
+        }
+        
+        System.out.println("Zarovnany zapis");
+        matrixOutput(A);
+        
+        
+        int sum;
+        double mean;
+        System.out.println("Prumerne reputace");
+        for (int i = 0; i < A.length; i++) {
+            sum=0;
+            for (int j = 0; j < A[i].length; j++) {
+                if(A[i]!=A[j]) {
+                    sum = A[i][j] + sum;
+                }
+            }
+            mean = (double)sum/(A[i].length-1);
+            System.out.println("Uzivatel "+ (i+1) + ":" + mean);
+        }
+        
+        kontroverzni(A);
+        
+        System.out.println("Pratelske dvojice:");
+        for (int i = 0; i < A.length; i++) {
+            for (int j = A.length-1; j > 0; j--) {
+                if(A[i][j] > 0 && A[j][i] > 0){
+                    System.out.println(i+","+j);
+                }
+            }
+            
+        }
+        
+        isPositive(A);
+        
+    }
+    
+    public static void kontroverzni(int[][] A){
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int[][] matrix = new int[A.length][A.length];
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j]=A[i][j];
+            }
+            
+        }
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if(matrix[i]!=matrix[j]) {
+                    if(matrix[i][j]<min)min=matrix[i][j];
+                    if(matrix[i][j]>max)max=matrix[i][j];
+                }
+            }
+            matrix[i][i] = max - min;
+            min = Integer.MAX_VALUE;
+            max = Integer.MIN_VALUE;
+        }
+        
+        int uzivatel = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            if(matrix[i][i]>max){
+                max = matrix[i][i];
+                uzivatel = i+1;
+            }
+        }
+        
+        System.out.println("Nejvice kontroverzni je uzivatel " + uzivatel + ". Ma rozdil " + max + ".");
+    }
+    
+    public static void isPositive(int[][] A){
+        boolean positive = false;
+        int[][]matrix = A;
+
+        for (int i = 0; i < matrix.length; i++) {
+            positive = false;
+            for (int j = 0; j < matrix[i].length; j++) {
+                
+                if(matrix[i][j] > 0){
+                    positive = true;
+                    break;
+                }
+                
+            }
+            if(!positive)break;
+        }
+        System.out.println("Platforma " + (positive==true?"je":"neni") + " pozitivni");
+    }
+    
+    public static void matrixOutput(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.format("%2d ",matrix[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");    
+    }
+    
+    
+}
