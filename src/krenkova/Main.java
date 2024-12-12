294b5bbf84a478dd06de821f9dda49e79a302971
Krenkova-M-b5b4 <Krenkova-M-b5b4@a2-14-mti.mshome.net>
2024-12-12 09:29:22 +0100

    testing push

---
+++ src/krenkova/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package krenkova;
+
+/**
+ *
+ * @author Krenkova-M-b5b4
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
