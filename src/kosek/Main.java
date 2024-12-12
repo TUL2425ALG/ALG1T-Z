487f0a089487076f5be13eab82068435b3f1777c
MiroslavKosek <miroslav.kosek@tul.cz>
2024-12-12 09:29:08 +0100

    init

---
+++ src/kosek/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kosek;
+
+/**
+ *
+ * @author Kosek-M-3a1f
+ */
+public class Main {
+    
+}
